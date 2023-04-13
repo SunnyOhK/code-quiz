296292c35d1b0b30b8425707293d76564d56916b
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-04-13 16:57:34 -0500

    worked with tutor to fix this mess

--- assets/JS/holdforlater.js
+++ assets/JS/holdforlater.js
@@ -48,72 +48,72 @@
 !! renderNextQuestion() !!
 
 
-// {
-//         question: "Which of the following is a function of the elephant's trunk?",
-//         answers: {
-//             a: "To suck up water for drinking and cooling",
-//             b: "To use as a snorkel",
-//             c: "To grab items that are too large to inhale",
-//             d: "To communicate (trumpet warnings and greet one another)",
-//             e: "All are true",
-//         },
-//         correctAnswer: "e"
-//     },
-//     {
-//         question: "On which continent is the elephant not native/ found in the wild?",
-//         answers: {
-//             a: "South America",
-//             b: "Africa",
-//             c: "Asia",
-//             d: "Elephants are native to all 3 continents",
-//             e: "Elephants are not native to any of these continents",
-//         },
-//         correctAnswer: "a"
-//     },
-    // {
-    //     question: "Which of the following is the largest living elephant species?",
-    //     answers: {
-    //         a: "Asian",
-    //         b: "African Savanna",
-    //         c: "African Forest",
-    //         d: "Woolly Mammoth",
-    //         e: "Mr. Snuffleupagus",
-    //     },
-    //     correctAnswer: "b"
-    // },
-    // {
-    //     question: "How much does a baby elephant normally weigh at birth?",
-    //     answers: {
-    //         a: "100 - 150 lbs",
-    //         b: "200 - 250 lbs",
-    //         c: "300 - 350 lbs",
-    //         d: "400 - 500 lbs",
-    //         e: "up to 800 lbs depending on the species",
-    //     },
-    //     correctAnswer: "b"
-    // },
-    // {
-    //     question: "Which of the following poses the greatest threat to elephants' survival?",
-    //     answers: {
-    //         a: "Large feline predators (ex. lions)",
-    //         b: "Declining birth rates",
-    //         c: "Humans (poaching and habitat destruction)",
-    //         d: "Mudslides",
-    //         e: "Elephant populations are not declining",
-    //     },
-    //     correctAnswer: "c"
-    // },
-    // {
-    //     question: "Which of the following is a common (true) claim about elephants?",
-    //     answers: {
-    //         a: "Elephants are afraid of mice",
-    //         b: "Elephants have the sharpest eyesight in the animal kingdom",
-    //         c: "Elephants love performing in the circus",
-    //         d: "An elephant a day keeps the doctor away",
-    //         e: "An elephant never forgets",
-    //     },
-    //     correctAnswer: "e"
-    // },
+{
+        question: "Which of the following is a function of the elephant's trunk?",
+        answers: {
+            a: "To suck up water for drinking and cooling",
+            b: "To use as a snorkel",
+            c: "To grab items that are too large to inhale",
+            d: "To communicate (trumpet warnings and greet one another)",
+            e: "All are true",
+        },
+        correctAnswer: "e"
+    },
+    {
+        question: "On which continent is the elephant not native/ found in the wild?",
+        answers: {
+            a: "South America",
+            b: "Africa",
+            c: "Asia",
+            d: "Elephants are native to all 3 continents",
+            e: "Elephants are not native to any of these continents",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which of the following is the largest living elephant species?",
+        answers: {
+            a: "Asian",
+            b: "African Savanna",
+            c: "African Forest",
+            d: "Woolly Mammoth",
+            e: "Mr. Snuffleupagus",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "How much does a baby elephant normally weigh at birth?",
+        answers: {
+            a: "100 - 150 lbs",
+            b: "200 - 250 lbs",
+            c: "300 - 350 lbs",
+            d: "400 - 500 lbs",
+            e: "up to 800 lbs depending on the species",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which of the following poses the greatest threat to elephants' survival?",
+        answers: {
+            a: "Large feline predators (ex. lions)",
+            b: "Declining birth rates",
+            c: "Humans (poaching and habitat destruction)",
+            d: "Mudslides",
+            e: "Elephant populations are not declining",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which of the following is a common (true) claim about elephants?",
+        answers: {
+            a: "Elephants are afraid of mice",
+            b: "Elephants have the sharpest eyesight in the animal kingdom",
+            c: "Elephants love performing in the circus",
+            d: "An elephant a day keeps the doctor away",
+            e: "An elephant never forgets",
+        },
+        correctAnswer: "e"
+    },
 
 
 USE THIS: 
---
+++ assets/JS/maybe.js
@@ -0,0 +1,91 @@
+var questionA = "Which of the following is a function of the elephant's trunk?";
+var answerChoicesA = ["To suck up water for drinking and cooling", "To use as a snorkel", "To grab items that are too large to inhale", "To communicate (trumpet warnings and greet one another)", "All are true"];
+var correctAnswerA = "All are true";
+
+var questionB = "On which continent is the elephant not native/ found in the wild?";
+var answerChoicesB = ["South America", "Africa", "Asia", "Elephants are native to all 3 continents", "Elephants are not native to any of these continents"];
+var correctAnswerB = "South America";
+
+var questionC = "Which of the following is the largest living elephant species?";
+var answerChoicesC = ["Asian", "African Savanna", "African Forest", "Woolly Mammoth", "Mr. Snuffleupagus"];
+var correctAnswerC = "African Savanna";
+
+var questionD = "How much does a baby elephant normally weigh at birth?";
+var answerChoicesD = ["100 - 150 lbs", "200 - 250 lbs", "300 - 350 lbs", "400 - 500 lbs", "up to 800 lbs depending on the species"];
+var correctAnswerD = "200 - 250 lbs";
+
+var questionE = "Which of the following poses the greatest threat to elephants' survival?";
+var answerChoiceE = ["Large feline predators (ex. lions)", "Declining birth rates", "Humans (poaching and habitat destruction)", "Mudslides", "Elephant populations are not declining"];
+var correctAnswerE = "Humans (poaching and habitat destruction)";
+
+var question6 = "Which of the following is a common (true) claim about elephants?";
+var answers6 = ["Elephants are afraid of mice", "Elephants have the sharpest eyesight in the animal kingdom", "Elephants love performing in the circus", "An elephant a day keeps the doctor away", "An elephant never forgets"];
+var correctAnswer6 = "An elephant never forgets";
+
+
+// CREATE VARIABLES FOR QUESTION CONTAINERS
+var questionContainer1 = document.getElementById("first");
+var questionContainer1 = document.getElementById("second");
+var questionContainer1 = document.getElementById("third");
+var questionContainer1 = document.getElementById("fourth");
+var questionContainer1 = document.getElementById("fifth");
+var questionContainer1 = document.getElementById("sixth");
+
+// ! IS THIS JSON FORMAT??
+// var questions = [
+    // {
+//         question: "Which of the following is a function of the elephant's trunk?",
+//         choice1: "To suck up water for drinking and cooling",
+//         choice2: "To use as a snorkel",
+//         choice3: "To grab items that are too large to inhale",
+//         choice4: "To communicate (trumpet warnings and greet one another)",
+//         choice5: "All are true",
+//         correct: 5;
+//     },
+//     {
+//         question: "On which continent is the elephant not native/ found in the wild?",
+//         choice1: "South America",
+//         choice2: "Africa",
+//         choice3: "Asia",
+//         choice4: "Elephants are native to all 3 continents",
+//         choice5: "Elephants are not native to any of these continents",
+//         correct: 1;
+//     },
+//     {
+//         question: "Which of the following is the largest living elephant species?",
+//         choice1: "Asian",
+//         choice2: "African Savanna",
+//         choice3: "African Forest",
+//         choice4: "Woolly Mammoth",
+//         choice5: "Mr. Snuffleupagus",
+//         correct: 2;
+//     },
+//     {
+//         question: "How much does a baby elephant normally weigh at birth?",
+//         choice1: "100 - 150 lbs",
+//         choice2: "200 - 250 lbs",
+//         choice3: "300 - 350 lbs",
+//         choice4: "400 - 500 lbs",
+//         choice5: "up to 800 lbs depending on the species",
+//         correct: 2;
+//     },
+//     {
+//         question: "Which of the following poses the greatest threat to elephants' survival?",
+//         choice1: "Large feline predators (ex. lions)",
+//         choice2: "Declining birth rates",
+//         choice3: "Humans (poaching and habitat destruction)",
+//         choice4: "Mudslides",
+//         choice5: "Elephant populations are not declining",
+//         correct: 3;
+//     },
+//     {
+//         question: "Which of the following is a common (true) claim about elephants?",
+//         choice1: "Elephants are afraid of mice",
+//         choice2: "Elephants have the sharpest eyesight in the animal kingdom",
+//         choice3: "Elephants love performing in the circus",
+//         choice4: "An elephant a day keeps the doctor away",
+//         choice5: "An elephant never forgets",
+//         correct: 5;
+//     },
+// ]
+
